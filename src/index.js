8b458f2f024acb650686a443de3efaa49ea41248
kbhooo <ironman035614@GMAIL.COM>
2021-05-07 19:30:34 +0900

    Modify:#1 COUNTER - 1.2 Actions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import { creatStore } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
-const number = document.getElementById("span");
+const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
-}
+const countModifier = (count = 0) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
 
 const countStore = creatStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
